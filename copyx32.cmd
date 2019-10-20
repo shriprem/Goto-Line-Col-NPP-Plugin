bb5632de43a9b90274a6099de3284a2bba937325
Shridhar Kumar <shriprem@gmail.com>
2019-10-19 23:51:21 -0400

    Including batch files for copying DLLs

---
+++ copyx32.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+SET BIN_DIR=bin
+SET NOTEPAD_DIR=E:\Portable\Notepad++
+
+COPY license.txt %BIN_DIR%
+COPY %BIN_DIR%\GotoLineCol.dll "%NOTEPAD_DIR%\plugins\GotoLineCol\"
+
+START /D "%NOTEPAD_DIR%" Notepa~1.exe
+EXIT 0
---
+++ copyx64.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+SET BIN_DIR=bin64
+SET NOTEPAD_DIR=E:\Portable\Notepad++x64
+
+COPY license.txt %BIN_DIR%
+COPY %BIN_DIR%\GotoLineCol.dll "%NOTEPAD_DIR%\plugins\GotoLineCol\"
+
+START /D "%NOTEPAD_DIR%" Notepa~1.exe
+EXIT 0

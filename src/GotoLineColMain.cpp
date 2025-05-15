a6b56ab4d12f0e63f8766a592814b16b3d442330
Shridhar Kumar <45252729+shriprem@users.noreply.github.com>
2025-05-15 19:59:28 -0400

    Remove unused files

--- src/GotoLineColMain.cpp
+++ src/GotoLineColMain.cpp
@@ -12,9 +12,10 @@
 //along with this program; if not, write to the Free Software
 //Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+
 #include "PluginDefinition.h"
 #include "Dialogs/GoToLineColPanel.h"
-#include "Darkmode/NPP_Plugin_Darkmode.h"
+
 
 extern FuncItem pluginMenuItems[MI_COUNT];
 extern NppData nppData;
